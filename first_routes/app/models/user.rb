959c7b3ff761bd381ddb20513ef2040ab14525a6
Onyi Cho <onyicho@gmail.com>
2019-06-03 17:32:49 -0700

    Finish Art Share project until seeding portion

--- first_routes/app/models/user.rb
+++ first_routes/app/models/user.rb
@@ -1,4 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  username   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord
-  validates :username, :email, presence: true, uniqueness: true
-  
+  validates :username, presence: true, uniqueness: true
+
+  has_many :artworks,
+    foreign_key: :artist_id,
+    class_name: :Artwork
+
+  has_many :shared_artworks,
+    through: :artworks, 
+    source: :shared_artworks
 end
--- first_routes/db/seeds.rb
+++ first_routes/db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Artwork.destroy_all
+ArtworkShare.destroy_all
+
+5.times { User.create(username: Faker::Name.unique.name) }
+
+5.times { Artwork.create(title: Faker::Name.unique.name, image_url: Faker::Internet.url , artist_id: User.all.sample.id) }
+
+
+def artwork_shared_helper
+  viewer_id = User.all.sample.id
+  artwork = Artwork.all.sample
+  viewer_id = User.all.sample.id until artwork.artist_id != viewer_id
+  return [viewer_id, artwork.id] 
+end
+i = 1
+until i == 5 do 
+    res = artwork_shared_helper
+    p res
+    if ArtworkShare.create(viewer_id: res[0], artwork_id: res[1])
+      i += 1
+    end
+end
+
